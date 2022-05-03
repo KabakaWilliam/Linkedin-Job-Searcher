37ade4df3a45c5bdf6525129eebd63c3de2cfa48
William Gitta <73100027+KabakaWilliam@users.noreply.github.com>
2022-05-03 19:28:54 +0300

    init commit

---
+++ main.py
@@ -0,0 +1,57 @@
+import logging
+from linkedin_jobs_scraper import LinkedinScraper
+from linkedin_jobs_scraper.events import Events, EventData
+from linkedin_jobs_scraper.query import Query, QueryOptions, QueryFilters
+from linkedin_jobs_scraper.filters import RelevanceFilters, TimeFilters, TypeFilters, ExperienceLevelFilters, RemoteFilters
+
+def on_data(data: EventData):
+    print('[ON_DATA]', data.title, data.company, data.company_link, data.date, data.link, data.insights, len(data.description), data.description)
+
+
+def on_error(error):
+    print('[ON_ERROR]', error)
+
+
+def on_end():
+    print('[ON_END]')
+
+
+scraper = LinkedinScraper(
+    chrome_executable_path=None, # Custom Chrome executable path (e.g. /foo/bar/bin/chromedriver) 
+    chrome_options=None,  # Custom Chrome options here
+    headless=True,  # Overrides headless mode only if chrome_options is None
+    max_workers=1,  # How many threads will be spawned to run queries concurrently (one Chrome driver for each thread)
+    slow_mo=1,  # Slow down the scraper to avoid 'Too many requests 429' errors (in seconds)
+)
+
+# Add event listeners
+scraper.on(Events.DATA, on_data)
+scraper.on(Events.ERROR, on_error)
+scraper.on(Events.END, on_end)
+
+queries = [
+    Query(
+        options=QueryOptions(
+            optimize=True,  # Blocks requests for resources like images and stylesheet
+            limit=5  # Limit the number of jobs to scrape,            
+        )
+    ),
+    Query(
+        query='Social Media Manager',
+        options=QueryOptions(
+            locations=['Qatar'],
+            optimize=False,
+            apply_link = True,  # Try to extract apply link (slower because it needs to open a new tab for each job). Default to false
+            limit=5,
+            filters=QueryFilters(              
+                # company_jobs_url='https://www.linkedin.com/jobs/search/?f_C=1441%2C17876832%2C791962%2C2374003%2C18950635%2C16140%2C10440912&geoId=92000000',  # Filter by companies
+                relevance=RelevanceFilters.RECENT,
+                time=TimeFilters.MONTH,
+                type=[TypeFilters.FULL_TIME, TypeFilters.INTERNSHIP],
+                experience=None,                
+            )
+        )
+    ),
+]
+
+scraper.run(queries)
